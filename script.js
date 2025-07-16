f5fd0628ed8ada149745d8e3d97b498f1eafda23
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2025-07-16 17:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+            // Create a new paragraph element
+            const message = document.createElement('p');
+            message.textContent = 'DOM load success';
+            
+            // Append it to the body
+            document.body.appendChild(message);
+        });
